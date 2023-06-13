332de79538f9299be07754a2b95ab1fb5c93eeb0
calvin-mcd <114294748+calvin-mcd@users.noreply.github.com>
2023-06-13 19:57:31 +0200

    added firmware (rotaries not tested yet)

---
+++ Firmware/Source Code/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x6D, 0x4C, 0x97, 0x54, 0x8B, 0xBE, 0x71, 0x77}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
---
+++ Firmware/Source Code/rules.mk
@@ -0,0 +1,3 @@
+# This file intentionally left blank
+ENCODER_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
